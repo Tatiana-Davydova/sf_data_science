dac3b8f5b5e4f3dd2d55238d66c98e953ba70940
Tatiana-Davydova <tatiana.davydova@internet.ru>
2022-11-09 21:32:30 +0300

    First commit

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,3 +1,4 @@
+#
 import numpy as np
 def random_predict(number:int=1) -> int:
     """Рандомно угадываем число
--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -9,28 +9,27 @@ def random_predict(number:int=1) -> int:
         int: Число попыток
     """
     count = 0
-    
+    predict_number = np.random.randint(1, 101) #предполагаемое число
+    max_number = 100
+    min_number = 1
+
+    my_list = range(min_number, max_number+1)
     while True:
-        count += 1
-        predict_number = np.random.randint(1, 101) #предполагаемое число
+        count += 1 
         if number == predict_number:
             break #выход из цикла, если угадали
         elif predict_number > number:
             max_number = predict_number
-            predict_number = max_number // 2
-            if predict_number < number:                
-                predict_number = math.ceil((max_number + predict_number) / 2)
-                    
-        elif predict_number < number: 
+            my_list = range(1, max_number+1)
+            predict_number = int((1 + max_number)/2)
+            
+        elif predict_number < number:
             min_number = predict_number
-            predict_number = math.ceil((100 + min_number) / 2) 
-            if predict_number > number:
-                predict_number = (min_number + predict_number) // 2
-            else:
-                max_number = predict_number
-                predict_number = math.ceil((max_number + min_number) / 2)
+            my_list = range(min_number, max_number+1) 
+            predict_number = int((min_number + max_number)/2)
     
     return(count)
+    
 print(f'Количество попыток:{random_predict()}')
 
 def score_game(random_predict) -> int:
